55366f5dc83fc9ea341ffb8b24482438d889cb22
Diego <dieho60714@gmail.com>
2025-05-02 12:17:21 -0500

    Refactor task table structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,44 +2,27 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatTableModule } from '@angular/material/table';
 
-interface tasksUser {
-  nameUser: string,
-  tasks: task[]
-};
-
-interface task {
-  name: string,
-  state: string
+interface Task {
+  name: string;
+  state: string;
 }
 
-const ELEMENT_DATA : task[] = [
-  {
-    name: "Task 1",
-    state: "Done"
-  },
-  {
-    name: "Task 2",
-    state: "In progress"
-  },
-  {
-    name: "Task 3",
-    state: "Pending"
-  },
+const TASK_DATA: Task[] = [
+  { name: "Task 1", state: "Done" },
+  { name: "Task 2", state: "In progress" },
+  { name: "Task 3", state: "Pending" },
 ];
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet, MatTableModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-
-
-
 export class AppComponent {
   title = 'prueba-tecnica';
 
-  displayedColumns: string[] = ['name-task', 'state-task'];
-
-  taskForUser = ELEMENT_DATA; 
-};
+  displayedColumns: string[] = ['name', 'state'];
+  dataSource = TASK_DATA;
+}
