aa248975e934a221ba019bebca86ec7a0a5f9faa
Diego <dieho60714@gmail.com>
2025-05-02 11:32:17 -0500

    Start table for task view by user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatTableModule } from '@angular/material/table';
+
+interface tasksUser {
+  nameUser: string,
+  tasks: task[]
+};
+
+interface task {
+  name: string,
+  state: string
+}
+
+const ELEMENT_DATA : task[] = [
+  {
+    name: "Task 1",
+    state: "Done"
+  },
+  {
+    name: "Task 2",
+    state: "In progress"
+  },
+  {
+    name: "Task 3",
+    state: "Pending"
+  },
+];
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatTableModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+
+
 export class AppComponent {
   title = 'prueba-tecnica';
-}
+
+  displayedColumns: string[] = ['name-task', 'state-task'];
+
+  taskForUser = ELEMENT_DATA; 
+};
